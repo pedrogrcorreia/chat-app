c7d719ab61d159f402bbc90ced060f029fb2d14d
Pedro Correia <pedrogrcorreia@gmail.com>
2023-03-21 19:13:24 +0000

    send messages to server and server emits to all clients

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,13 @@
-io()
+const socket = io()
+
+const form = document.querySelector('form')
+const message = document.querySelector('input')
+
+socket.on('message', (text) => {
+    console.log(text)
+})
+
+document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    socket.emit('sendMessage', message.value.toString())
+})
--- src/index.js
+++ src/index.js
@@ -13,9 +13,18 @@ const publicPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicPath))
 
-io.on('connection', () => {
+let count = 0
+
+io.on('connection', (socket) => {
     console.log('New WebSocket connection')
+
+    socket.emit('message', 'Welcome!')
+
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message)
+    })
 })
+
 
 
 server.listen(port, () => {
