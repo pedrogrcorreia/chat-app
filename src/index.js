5b8d284c5b08146f7fadc38f04631c387f1d654f
Pedro Correia <pedrogrcorreia@gmail.com>
2023-03-21 18:47:59 +0000

    setup express server for socket io

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
 const port = process.env.PORT || 3000
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 const publicPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicPath))
 
-
 app.get('/', (req, res) => {
     res.send()
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log('Server is running on port', port)
 })
